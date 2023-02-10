ba4ff500efe671a9bc08f43e9840f1a334c6e600
lauritsbonde <lauritsbonde@hotmail.dk>
2023-02-10 13:57:21 +0100

    Frontend and backend is setup
    Co-authored-by: Adrian Bay Dorph <adriandorph@users.noreply.github.com>
    Co-authored-by: Anton Marius Breinholt <antbrr@users.noreply.github.com>
    Co-authored-by: NebelWeden <NebelWeden@users.noreply.github.com>
    Co-authored-by: Markus Johansen <KusMar00@users.noreply.github.com>

---
+++ refactored/Backend/Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+namespace Test;
+
+public class UnitTest1
+{
+    public UnitTest1()
+    {
+        // Before each test, set up a blank database
+        
+    }
+
+    [Fact]
+    public void test_register()
+    {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void test_login_logout()
+    {
+        
+    }
+    
+    [Fact]
+    public void test_message_recording()
+    {
+        
+    }
+    
+
+    [Fact]
+    public void test_timelines()
+    {
+        
+    }
+}
