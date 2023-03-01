8558276cf69b1713a4d94dd456108781057517b2
lauritsbonde <lauritsbonde@hotmail.dk>
2023-03-02 00:16:38 +0100

    this works, just figure out how to share the token in auth0

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,8 @@
 using Backend.Infrastructure;
 using Backend.Core.EF;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -20,7 +22,31 @@ builder.Services.AddCors(options => {
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MinitwitApi", Version = "v1.0.0" });
+
+        //👇 new code
+        var securitySchema = new OpenApiSecurityScheme
+        {
+          Description = "Using the Authorization header with the Bearer scheme.",
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.Http,
+          Scheme = "bearer",
+          Reference = new OpenApiReference
+          {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+          }
+        };
+
+        c.AddSecurityDefinition("Bearer", securitySchema);
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+          {
+              { securitySchema, new[] { "Bearer" } }
+          });
+});
 
 builder.Services.AddDbContext<MinitwitContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("Minitwit"))
