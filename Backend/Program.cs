0c0771323785bc2effcf66366c0686647a2e8b66
lauritsbonde <lauritsbonde@hotmail.dk>
2023-03-21 18:04:10 +0100

    started on logging

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,6 +3,13 @@ using Backend.Core.EF;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 using Prometheus;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +24,32 @@ builder.Services.AddCors(options => {
         policy.AllowAnyMethod();
     });
 });
+
+// Logging
+
+ConfigureLogging();
+
+try
+	{
+		Host.CreateDefaultBuilder(args)
+		.ConfigureWebHostDefaults(webBuilder =>
+		{
+			webBuilder.UseStartup<Program>();
+		})
+		.ConfigureAppConfiguration(configuration =>
+		{
+			configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+			configuration.AddJsonFile(
+				$"appsettings.json",
+				optional: true);
+		})
+		.UseSerilog();
+	}
+	catch (System.Exception ex)
+	{
+		Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+		throw;
+	}
 
 // Add services to the container.
 
@@ -102,3 +135,34 @@ app.MapControllers();
 app.MapMetrics();
 
 app.Run();
+
+
+void ConfigureLogging()
+{
+    var environment = "Development";
+	var configuration = new ConfigurationBuilder()
+		.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+		.AddJsonFile(
+			$"appsettings.json",
+			optional: true)
+		.Build();
+
+	Log.Logger = new LoggerConfiguration()
+		.Enrich.FromLogContext()
+		.Enrich.WithMachineName()
+		.WriteTo.Debug()
+		.WriteTo.Console()
+		.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+		.Enrich.WithProperty("Environment", environment)
+		.ReadFrom.Configuration(configuration)
+		.CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+{
+	return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+	{
+		AutoRegisterTemplate = true,
+		IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+	};
+}
